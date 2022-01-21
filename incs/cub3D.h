87e7dfaef1bf222e9f67cc992bd54b8783fb2c37
realnine <ljk0252g@gmail.com>
2022-01-21 13:10:57 +0900

    1:10

--- incs/cub3D.h
+++ incs/cub3D.h
@@ -15,13 +15,16 @@
 
 # include <math.h>
 # include <stdio.h>
+# include <X11/X.h>
 
-// # include <X11/X.h>
+/*
 # include "/Applications/Xcode.app/Contents/Developer/Platforms/\
 MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/Library/\
 Frameworks/Tk.framework/Versions/8.5/Headers/X11/X.h"
+*/
 
 # include <stdbool.h>
+
 # include "mlx.h"
 # include "keys.h"
 # include "libft.h"
